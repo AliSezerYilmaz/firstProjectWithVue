7d13794d9f60d181f032ab16c9c14dec21300d7b
AliSezerYilmaz <aliszr1903@gmail.com>
2022-06-30 12:54:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ProductAdd @add:product="addProduct" />
+    <ProductList
+      @update:product="updateProduct"
+      @delete:product="deleteProduct"
+      :products="products"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from "./components/ProductList.vue";
+import ProductAdd from "./components/ProductAdd.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    //componentler burda tanımlanır
+    ProductList,
+    ProductAdd,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      const result = await fetch("http://localhost:3000/products");
+      const data = await result.json();
+      this.products = data;
+    },
+    async deleteProduct(product) {
+      await fetch("http://localhost:3000/products/" + product.id, {
+        method: "DELETE",
+      });
+      this.getProducts()
+    },
+    async updateProduct(product) {
+       await fetch(
+        "http://localhost:3000/products/" + product.id,
+        {
+          method: "PUT",
+          body: JSON.stringify(product),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      this.getProducts()
+
+    },
+    async addProduct(product) {
+       await fetch("http://localhost:3000/products", {
+        method: "POST",
+        body: JSON.stringify(product),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      this.getProducts()
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,7 +70,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/ProductAdd.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="addForm">
+    <form @submit.prevent="handleAdd" class="form-inline">
+      <div class="form-group listxx">
+        <label for="id">Id</label>
+        <input v-model="product.id" type="text" class="form-control" id="id"/>
+      </div>
+      <div class="form-group listxx">
+        <label for="productName">Ürün Adı</label>
+        <input v-model="product.productName" type="text" class="form-control" id="productName"/>
+      </div>
+      <div class="form-group listxx">
+        <label for="categoryId">Kategori</label>
+        <input v-model="product.categoryId" type="text" class="form-control" id="categoryId"/>
+      </div>
+      <div class="form-group listxx">
+        <label for="quantityPerUnit">Açıklama</label>
+        <input v-model="product.quantityPerUnit" type="text" class="form-control" id="quantityPerUnit"/>
+      </div>
+      <div class="form-group listxx">
+        <label for="unitPrice">Fiyat</label>
+        <input v-model="product.unitPrice" type="text" class="form-control" id="unitPrice"/>
+      </div>
+      <div class="form-group listxx">
+        <label for="unitsInStock">Stok Adedi</label>
+        <input v-model="product.unitsInStock" type="text" class="form-control" id="unitsInStock"/>
+      </div>
+      <button type="submit" class="btn btn-sm btn-success">Ekle</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "product-add",
+  data() {
+    return {
+      product: {
+        id: "",
+        categoryId: "",
+        productName: "",
+        quantityPerUnit: "",
+        unitPrice: "",
+        unitsInStock: "",
+      },
+    };
+  },
+  methods:{
+    handleAdd(){
+        this.$emit("add:product",this.product)
+    }
+  }
+};
+</script>
+<style scoped>
+.form-inline {  
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+}
+.form-inline label {
+  margin: 5px 10px 5px 0;
+}
+.form-inline input {
+  vertical-align: middle;
+  margin: 5px 10px 5px 0;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+}
+.form-inline button {
+  padding: 10px 20px;
+  background-color: dodgerblue;
+  border: 1px solid #ddd;
+  color: white;
+  cursor: pointer;
+}
+@media (max-width: 800px) {
+  .form-inline input {
+    margin: 10px 0;
+  }
+  
+  .form-inline {
+    flex-direction: column;
+    align-items: stretch;
+  }
+}
+
+</style>
